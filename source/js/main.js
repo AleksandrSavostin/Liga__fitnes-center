80db5b875f2d2d8eeb4ed298a64e36770dcae66d
Aleksandr <asavostin1@gmail.com>
2020-01-23 01:58:31 +0500

    футер

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,51 @@
 'use strict';
 
 (function () {
+var mySwiper = new Swiper('.swiper-container', {
+    speed: 400,
+    slidesPerView: 4,
+    spaceBetween: 40,
+    breakpoints: {
+      767: {
+        slidesPerView: 1,
+        spaceBetween: 10
+      },
+      1199: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      1366: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    },
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
+var mySwiper2 = new Swiper('.swiper-container-s2', {
+    speed: 400,
+    slidesPerView: 4,
+    spaceBetween: 40,
+    breakpoints: {
+      767: {
+        slidesPerView: 1,
+        spaceBetween: 10
+      },
+      1199: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      1366: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    },
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 })();
