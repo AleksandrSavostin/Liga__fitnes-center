6029b1f7d9a7c222351af42114b0bc41352f5578
Aleksandr <asavostin1@gmail.com>
2020-01-24 02:38:19 +0500

    футер

--- build/js/main.js
+++ build/js/main.js
@@ -1,51 +1,37 @@
 'use strict';
 
 (function () {
-var mySwiper = new Swiper('.swiper-container', {
-    speed: 400,
-    slidesPerView: 4,
-    spaceBetween: 40,
-    breakpoints: {
-      767: {
-        slidesPerView: 1,
-        spaceBetween: 10
-      },
-      1199: {
-        slidesPerView: 2,
-        spaceBetween: 30
-      },
-      1366: {
-        slidesPerView: 4,
-        spaceBetween: 40
-      }
+var mySwiper = new Swiper('.swiper-container__s1', {
+  speed: 400,
+  slidesPerView: 4,
+  spaceBetween: 40,
+  breakpoints: {
+    767: {
+      slidesPerView: 1,
+      spaceBetween: 10
     },
-    navigation: {
+    1199: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    1366: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  },
+  navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-  },
+  }
 });
 
-var mySwiper2 = new Swiper('.swiper-container-s2', {
-    speed: 400,
-    slidesPerView: 4,
-    spaceBetween: 40,
-    breakpoints: {
-      767: {
-        slidesPerView: 1,
-        spaceBetween: 10
-      },
-      1199: {
-        slidesPerView: 2,
-        spaceBetween: 30
-      },
-      1366: {
-        slidesPerView: 4,
-        spaceBetween: 40
-      }
-    },
-    navigation: {
+var mySwiper2 = new Swiper('.swiper-container__s2', {
+  speed: 400,
+  slidesPerView: 1,
+  spaceBetween: 40,
+  navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-  },
-});
+  }
+})
 })();
