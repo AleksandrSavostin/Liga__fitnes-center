eb5708f687d2c1a96909fd92e030e1c649903e76
Aleksandr <asavostin1@gmail.com>
2020-01-27 23:04:53 +0500

    Тренеры

--- build/js/main.js
+++ build/js/main.js
@@ -4,18 +4,22 @@
 var mySwiper = new Swiper('.swiper-container-s1', {
   speed: 400,
   slidesPerView: 4,
+  slidesPerGroup: 4,
   spaceBetween: 40,
   breakpoints: {
     767: {
       slidesPerView: 1,
+      slidesPerGroup: 1,
       spaceBetween: 10
     },
     1199: {
       slidesPerView: 2,
+      slidesPerGroup: 2,
       spaceBetween: 30
     },
     1366: {
       slidesPerView: 4,
+      slidesPerGroup: 4,
       spaceBetween: 40
     }
   },
